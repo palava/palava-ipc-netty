0d1696cbfdfc38d5c927851b55127b70621591dd
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-05-21 18:37:28 +0000

    added license headers

--- src/main/java/de/cosmocode/palava/ipc/netty/ProtocolHandlerModule.java
+++ src/main/java/de/cosmocode/palava/ipc/netty/ProtocolHandlerModule.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.palava.ipc.netty;
 
 import java.lang.annotation.Annotation;
@@ -5,6 +21,7 @@ import java.lang.annotation.Annotation;
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 import com.google.inject.Key;
+import com.google.inject.Module;
 import com.google.inject.PrivateModule;
 import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
@@ -24,7 +41,7 @@ public final class ProtocolHandlerModule extends PrivateModule {
     private final Class<? extends Annotation> annotation;
 
     @Inject
-    public ProtocolHandlerModule(Class<? extends Annotation> annotation) {
+    private ProtocolHandlerModule(Class<? extends Annotation> annotation) {
         this.annotation = Preconditions.checkNotNull(annotation, "Annotation");
     }
 
@@ -33,6 +50,20 @@ public final class ProtocolHandlerModule extends PrivateModule {
         bind(literal).to(Key.get(literal, annotation)).in(Singleton.class);
         bind(ProtocolHandler.class).annotatedWith(annotation).to(ProtocolHandler.class).in(Singleton.class);
         expose(ProtocolHandler.class).annotatedWith(annotation);
+    }
+    
+    /**
+     * Creates a Module which binds a {@link ProtocolHandler} using the specifed
+     * {@link Annotation} type. The bound protocol handler depends on an {@link Iterable}
+     * of {@link Protocol}s bound with the same annotation.
+     * 
+     * @since 1.0
+     * @param annotation the binding annotation
+     * @return a {@link Module} used for binding a {@link ProtocolHandler}
+     * @throws NullPointerException if annotation is null
+     */
+    public static Module annotatedWith(Class<? extends Annotation> annotation) {
+        return new ProtocolHandlerModule(annotation);
     }
 
 }
